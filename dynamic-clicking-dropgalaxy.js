44a0aa93815c9d4f437b02c55ac124cb2759ae39
Jay <42314425+JethaLal420@users.noreply.github.com>
2021-12-24 21:48:29 +0530

    Create dynamic-clicking-dropgalaxy.js

---
+++ dynamic-clicking-dropgalaxy.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         DropGalaxy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       JethaLal_420
+// @match        https://dropgalaxy.com/*
+// @icon         https://www.google.com/s2/favicons?domain=dropgalaxy.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var adBlock = document.getElementById('adblock_detected')
+    var adCheck = document.getElementById('adblock_check')
+    if (adBlock) {
+        adBlock.remove()
+    }
+    if (adCheck) {
+        adCheck.remove()
+    }
+
+    setTimeout(() => {
+        var dwnBtn = document.getElementsByClassName('fa-cloud-download')[0]
+        if (dwnBtn) {
+        dwnBtn.click()
+        }
+        var createBtn = document.getElementById('downloadBtnClick')
+        if (createBtn) {
+        createBtn.disabled = false
+        createBtn.click()
+        }
+        /*var finalDbtn = document.getElementById('dllink')
+
+        var link = finalDbtn.getAttribute('action')
+        console.log(link)
+
+        var hh = window.open(link, '_blank')*/
+    }, 3000)
+})();
